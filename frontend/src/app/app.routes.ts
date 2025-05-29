23d0c43b67e16711cdb718c6f5a1c9da8808dbff
cowhorse7 <rnc23@byu.edu>
2025-05-29 10:22:36 -0600

    basic home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthErrorPage, AuthCallbackPage, NotFoundPage, ForbiddenPage } from '@fhss-web-team/frontend-utils';
+import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
   { path: 'forbidden', component: ForbiddenPage },
   { path: 'auth-callback', component: AuthCallbackPage },
   { path: 'auth-error', component: AuthErrorPage },
-  { path: '', component: NotFoundPage }, // Change this to be the home page
+  { path: '', component: HomePage }, 
   { path: '**', component: NotFoundPage },
 ];
