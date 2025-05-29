ffc257d5f670b4338455931b86ffd8cc3c65a0fc
cowhorse7 <rnc23@byu.edu>
2025-05-29 12:02:28 -0600

    Homepage Creation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthErrorPage, AuthCallbackPage, NotFoundPage, ForbiddenPage } from '@fhss-web-team/frontend-utils';
 import { HomePage } from './pages/home/home.page';
+import { ByuLayout } from './layouts/byu/byu.layout';
 
 export const routes: Routes = [
   { path: 'forbidden', component: ForbiddenPage },
   { path: 'auth-callback', component: AuthCallbackPage },
   { path: 'auth-error', component: AuthErrorPage },
-  { path: '', component: HomePage }, 
+  { path: '', component: ByuLayout, children: [ {path: '', component: HomePage} ],}, 
   { path: '**', component: NotFoundPage },
 ];
