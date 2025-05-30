98d1ec4d68ec513facb4c7860abfc88a104b4452
cowhorse7 <rnc23@byu.edu>
2025-05-30 10:58:22 -0600

    Finished p6, bug fixes

--- frontend/src/app/pages/decks/decks.page.ts
+++ frontend/src/app/pages/decks/decks.page.ts
@@ -1,9 +1,21 @@
-import { Component, inject, linkedSignal, signal } from '@angular/core';
+import { Component, computed, inject, linkedSignal, signal } from '@angular/core';
 import { DecksService } from '../../services/decks/decks.service';
+import { PageEvent } from '@angular/material/paginator';
+import { debounced } from '@fhss-web-team/frontend-utils';
+import { Sort } from '@angular/material/sort';
+import {MatInputModule} from '@angular/material/input';
+import {MatSortModule} from '@angular/material/sort';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { FormsModule } from '@angular/forms';
+
+type SortOptions = 'name' | 'cards' | 'updatedAt' | 'createdAt'
 
 @Component({
   selector: 'app-decks',
-  imports: [],
+  imports: [MatInputModule, MatSortModule, MatCardModule, MatIconModule, MatPaginatorModule, MatProgressSpinnerModule, FormsModule],
   templateUrl: './decks.page.html',
   styleUrl: './decks.page.scss'
 })
@@ -23,4 +35,20 @@ export class DecksPage {
   sortDirection = signal<'asc' | 'desc'>(this.defaultSortDirection);
 
   service = inject(DecksService);
+  decksRequest = this.service.getDecks(
+    debounced(this.search),
+    this.sortBy,
+    this.sortDirection,
+    this.PAGE_SIZE,
+    computed(() => this.pageIndex() * this.PAGE_SIZE),
+  );
+
+  handlePaginationUpdate(e:PageEvent) {
+    this.pageIndex.set(e.pageIndex);
+  }
+  onSort(sort:Sort){
+    this.sortBy.set((sort.active || this.defaultSortBy) as SortOptions);
+    this.sortDirection.set(sort.direction || this.defaultSortDirection);
+    this.pageIndex.set(0);
+  }
 }
